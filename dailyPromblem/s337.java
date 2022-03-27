4c18ced7263f57fecb2526fde7f731d2994a54a3
iUshio <97095966+iUshio@users.noreply.github.com>
2022-03-27 17:56:45 +0800

    Create s337.java

---
+++ dailyPromblem/s337.java
@@ -0,0 +1,70 @@
+package dailyPromblem;
+
+import java.util.*;
+
+/**
+ * s337
+ */
+public class s337 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 使用map对已访问过的节点跳过
+    public int rob1(TreeNode root) {
+        Map<TreeNode, Integer> memo = new HashMap<>();
+        return robAction(root, memo);
+    }
+
+    int robAction(TreeNode root, Map<TreeNode, Integer> memo) {
+        if (root == null)
+            return 0;
+        if (memo.containsKey(root))
+            return memo.get(root);
+        int money = root.val;
+        // 如果选中该节点的话直接跳过该节点
+        if (root.left != null) {
+            money += robAction(root.left.left, memo) + robAction(root.left.right, memo);
+        }
+        if (root.right != null) {
+            money += robAction(root.right.left, memo) + robAction(root.right.right, memo);
+        }
+        int res = Math.max(money, robAction(root.left, memo) + robAction(root.right, memo));
+        memo.put(root, res);
+        return res;
+    }
+
+    // 动态规划
+    public int rob3(TreeNode root) {
+        int[] res = robAction1(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    int[] robAction1(TreeNode root) {
+        int res[] = new int[2];
+        if (root == null)
+            return res;
+
+        int[] left = robAction1(root.left);
+        int[] right = robAction1(root.right);
+
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = root.val + left[0] + right[0];
+        return res;
+    }
+}
