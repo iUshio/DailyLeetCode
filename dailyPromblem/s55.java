897b48ad73d473027fc2db25e02efb6d146cb7a8
Ushio <1394598567@qq.com>
2022-07-25 11:42:12 +0800

    2022_07_25 55.跳跃游戏

--- dailyPromblem/s55.java
+++ dailyPromblem/s55.java
@@ -3,28 +3,19 @@ package dailyPromblem;
 public class s55 {
     public boolean canJump(int[] nums) {
         int len = nums.length;
-        if( len == 1){
+        if(len == 1){
             return true;
         }
-
-        int far = 0;
-        int tmp = 0;
-        for(int i = 0; i < len; i++){
-            tmp = i + nums[i];
-            far = Math.max(far,tmp);
-            if(far == i){
-                // 该位置为最远位置
-                if(far < len - 1){
-                    return false;
-                }else{
-                    return true;
-                }
-            }
-            if(far >= len - 1){
+        // 记录最远能到达的位置
+        int distance = nums[0];
+        // 只访问到distance
+        for(int i = 0; i <= distance; i++){
+            distance = Math.max(distance,i + nums[i]);
+            // 到达目的地
+            if(distance >= len - 1){
                 return true;
             }
         }
-
         return false;
     }
 }
