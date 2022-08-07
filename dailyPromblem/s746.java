bd26219c8fa61e04815982711247922fe6cdb61d
Ushio <1394598567@qq.com>
2022-08-07 16:33:17 +0800

    2022_08_07 746.使用最小花费爬楼梯

---
+++ dailyPromblem/s746.java
@@ -0,0 +1,14 @@
+package dailyPromblem;
+
+public class s746 {
+    public int minCostClimbingStairs(int[] cost) {
+        int len = cost.length;
+        int[] dp = new int[len + 1];
+
+        for(int i = 2; i <= len; i++){
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1],dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[len];
+    }
+}
