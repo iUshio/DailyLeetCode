87f6611d5a79de648a3df0ece6f5326747471c0f
Ushio <1394598567@qq.com>
2022-08-21 12:38:27 +0800

    2022_08_21 31.下一个排列

---
+++ dailyPromblem/s31.java
@@ -0,0 +1,37 @@
+package dailyPromblem;
+
+public class s31 {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        // 去除从左往右单调递减的序列
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            // 在i之后的数字是单调递减的，寻找第一个大于i的数字
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+}
