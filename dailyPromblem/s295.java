6e2a5f3c193df17f56442e13b4d048ebaee557ea
Ushio <1394598567@qq.com>
2022-09-05 11:13:16 +0800

    2022_09_05 295.数据流的中位数

---
+++ dailyPromblem/s295.java
@@ -0,0 +1,38 @@
+package dailyPromblem;
+
+import java.util.*;
+
+public class s295 {
+
+    class MedianFinder {
+        PriorityQueue<Integer> queMin;
+        PriorityQueue<Integer> queMax;
+
+        public MedianFinder() {
+            queMin = new PriorityQueue<Integer>((a, b) -> (b - a));
+            queMax = new PriorityQueue<Integer>((a, b) -> (a - b));
+        }
+
+        public void addNum(int num) {
+            // 保证总数为偶数时，min和max数量一样，否则min比max多一个
+            if (queMin.isEmpty() || num <= queMin.peek()) {
+                queMin.offer(num);
+                if (queMax.size() + 1 < queMin.size()) {
+                    queMax.offer(queMin.poll());
+                }
+            } else {
+                queMax.offer(num);
+                if (queMax.size() > queMin.size()) {
+                    queMin.offer(queMax.poll());
+                }
+            }
+        }
+
+        public double findMedian() {
+            if (queMin.size() > queMax.size()) {
+                return queMin.peek();
+            }
+            return (queMin.peek() + queMax.peek()) / 2.0;
+        }
+    }
+}
